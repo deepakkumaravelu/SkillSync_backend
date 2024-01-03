a5f54a39eaaa769aa1bd4c6373a277aed0e84c56
Deepak Kumaravelu <67898647+deepakkumaravelu@users.noreply.github.com>
2024-01-03 15:20:22 +0530

    login query

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,2 +1,15 @@
-export const fn =(req,res)=>{
+import User from "../models/user.model.js"
+import jwt from "jsonwebtoken";
+export const deleteUser =async(req,res)=>{
+    const user = await User.findById(req.params.id);
+    const token = req.cookies.accessToken;
+    if(!token) return res.status(401).send("you are not authenticated");
+
+ jwt.verify(token,process.env.JWT_KEY,(err,payload)=>{
+    if(payload.id !== user._id){
+
+    }
+ });
+
+    // await User.findByIdAndDelete(req.params.id);
 }
