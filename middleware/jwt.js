c1983d43bc4f44bae964ee9d2234615dea598791
deepakkumaravelu <deepak1122003kumar@gmail.com>
2024-01-08 15:25:51 +0530

    env dlt

---
+++ middleware/jwt.js
@@ -0,0 +1,11 @@
+export const verifyToken =(req,res)=>{
+    const token = req.cookies.accessToken;
+    if(!token) return res.status(401).send("you are not authenticated");
+    jwt.verify(token,process.env.JWT_KEY,async (err,payload)=>{
+        if(payload.id !== user._id.toString()){
+          return res.status(403).send("you can delete only your account");
+        }
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).send("deleted");
+     });
+}
