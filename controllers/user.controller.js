1b3b4a91b9bff328c06fdd5a6c83718693536eda
deepakkumaravelu <deepak1122003kumar@gmail.com>
2024-01-08 14:56:39 +0530

    user delete updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,11 +5,12 @@ export const deleteUser =async(req,res)=>{
     const token = req.cookies.accessToken;
     if(!token) return res.status(401).send("you are not authenticated");
 
- jwt.verify(token,process.env.JWT_KEY,(err,payload)=>{
-    if(payload.id !== user._id){
-      res.status(403).send("you are not authenticated");
+ jwt.verify(token,process.env.JWT_KEY,async (err,payload)=>{
+    if(payload.id !== user._id.toString()){
+      return res.status(403).send("you can delete only your account");
     }
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).send("deleted");
  });
 
-    // await User.findByIdAndDelete(req.params.id);
 }
