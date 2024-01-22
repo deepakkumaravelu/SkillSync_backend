cec9102d30d6deeb009bbcc9611c720622ba3662
deepakkumaravelu <deepak1122003kumar@gmail.com>
2024-01-22 16:04:27 +0530

    createGig function updated

--- controllers/gig.controller.js
+++ controllers/gig.controller.js
@@ -1,4 +1,19 @@
+import createError from "../utils/createError.js"
+
 export const createGig = async(req,res,next)=>{
+    if(!req.isSeller){
+        return next(createError(403,"Only sellers can create a gig"));
+    }
+    const newGig =new Gig({
+        userId:req.userId,
+        ...req.body,
+    });
+    try{
+        const savedGig = await newGig.save();
+        res.status(201).json(savedGig);
+    }catch(err){
+        next(err);
+    }
 }
 export const deleteGig = async(req,res,next)=>{
 }
@@ -9,6 +24,3 @@ export const getGig = async(req,res,next)=>{
 export const getGigs = async(req,res,next)=>{
 }
 
-
-
-//1:37:14
